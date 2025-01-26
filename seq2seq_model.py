96ca16bd80f3d60658f9679bc138e87861fc5790
Pedro Lyra <pedrolyrac@gmail.com>
2025-01-26 17:02:20 -0300

    feat: implemented seq2seq model

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -4,12 +4,13 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.nn.functional import embedding
+from torch.onnx.symbolic_opset11 import unsqueeze
 from torchtext.datasets import Multi30k
 from torchtext.data import Field, BucketIterator
 import numpy as np
 import spacy
 import random
-from torch.utils.tensorboard import SummaryWriter
+import treatment
 
 class Encoder(nn.Module):
     def __init__(self, input_size, embedding_size, hidden_size, num_layers, dropout_rate):
@@ -26,3 +27,45 @@ class Encoder(nn.Module):
         outputs, (hidden, cell) = self.rnn(embedding)
 
         return hidden, cell
+
+class Decoder(nn.Module):
+    def __init__(self, input_size, embedding_size, hidden_size, output_size, num_layers, dropout_rate):
+        super(Decoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.dropout = nn.Dropout(dropout_rate)
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=dropout_rate)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, hidden, cell):
+        x = unsqueeze(0)
+        embedding = self.dropout(self.embedding(x))
+        outputs, (hidden, cell) = self.rnn(embedding, (hidden, cell))
+        predictions = self.fc(outputs)
+        predictions = predictions.squeeze(0)
+
+        return predictions, hidden, cell
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(Seq2Seq, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, source, target, teacher_force_ratio=0.5):
+        batch_size = source.shape[1]
+        target_len = target.shape[0]
+        target_vocab_size = len(treatment.english.vocab)
+
+        outputs = torch.zeros(target_len, batch_size. target_vocab_size).to(device)
+        hidden, cell = self.encoder(source)
+        x = target[0]
+
+        for t in range(1, target_len):
+            output, hidden, cell = self.decoder(x, hidden, cell)
+            outputs[t] = output
+            best_guess = output.argmax(1)
+            x = target[t] if random.random() < teacher_force_ratio else best_guess
+        return outputs
