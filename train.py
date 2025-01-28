d7048c2a7eaa84e4f4d0f2df21d5045f7a2a39bc
Pedro Lyra <pedrolyrac@gmail.com>
2025-01-28 15:15:15 -0300

    fix: fixed errors in train.py

--- train.py
+++ train.py
@@ -1,18 +1,11 @@
-from lib2to3.pgen2.tokenize import tokenize
-
-import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.nn.functional import embedding
-from torch.onnx.symbolic_opset11 import unsqueeze
 from torch.utils.tensorboard import SummaryWriter
-from torchtext.datasets import Multi30k
 from torchtext.data import Field, BucketIterator
-import numpy as np
-import spacy
-import random
 import treatment
 from seq2seq_model import Decoder, Encoder, Seq2Seq
+from treatment import english, german, test_data
+from utils import *
 
 # Training Hyperparameters
 num_epochs = 20
@@ -21,9 +14,9 @@ batch_size = 64
 
 # Model Hyperparameters
 load_model = False
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 input_size_encoder = len(treatment.german.vocab)
 input_size_decoder = len(treatment.german.vocab)
+output_size = len(english.vocab)
 encoder_embedding_size = 300
 decoder_embedding_size = 300
 hidden_size = 1024
@@ -44,7 +37,14 @@ train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
 )
 
 encoder_net = Encoder(input_size_encoder, encoder_embedding_size, hidden_size, num_layers, encoder_dropout).to(device)
-decoder_net = Decoder(input_size_decoder, decoder_embedding_size, hidden_size, num_layers, decoder_dropout).to(device)
+decoder_net = Decoder(
+    input_size_decoder,
+    decoder_embedding_size,
+    hidden_size,
+    num_layers,
+    output_size,
+    decoder_dropout
+).to(device)
 
 model = Seq2Seq(encoder_net, decoder_net).to(device)
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -62,16 +62,18 @@ for epoch in range(num_epochs):
     save_checkpoint(checkpoint)
 
     model.eval()
-    translated_sentence = translate_sentence(model, sentence, german, english, device, max_len=50)
+    translated_sentence = translate_sentence(model, sentence, german, english, device, max_length=50)
 
     for batch_idx, batch in enumerate(train_iterator):
         input_data = batch.src.to(device)
         target_data = batch.trg.to(device)
 
-        output = output[1:].reshape(-1, output.shape[2])
-        target = target[1:].reshape(-1)
+        output = model(input_data, target_data)
 
-        optimer.zero_grad()
+        output = output[1:].reshape(-1, output.shape[2])
+        target = target_data[1:].reshape(-1)
+
+        optimizer.zero_grad()
         loss = criterion(output, target)
 
         loss.backward()
