7515c18a78e2abfb7bf3800bc322bdba8217fce4
Pedro Lyra <pedrolyrac@gmail.com>
2025-01-28 15:09:48 -0300

    refactor: changed some parameter names in utils

--- utils.py
+++ utils.py
@@ -5,14 +5,14 @@ from torchtext.data.metrics import bleu_score
 sentence = 'ein boot mit mehreren mannern darauf wird von einem groben pferdegespann ans ufer gezogen.'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def translate_sentence(model, sentence, german, english, device, max_length=50):
+def translate_sentence(model, used_sentence, german, english, used_device, max_length=50):
     """
     Make the transalation of a sentence
     :param model:
-    :param sentence:
+    :param used_sentence:
     :param german:
     :param english:
-    :param device:
+    :param used_device:
     :param max_length:
     :return:
     """
@@ -20,10 +20,10 @@ def translate_sentence(model, sentence, german, english, device, max_length=50):
     spacy_ger = spacy.load("de")
 
     # Create tokens using spacy and everything in lower case (which is what our vocab is)
-    if type(sentence) == str:
-        tokens = [token.text.lower() for token in spacy_ger(sentence)]
+    if type(used_sentence) == str:
+        tokens = [token.text.lower() for token in spacy_ger(used_sentence)]
     else:
-        tokens = [token.lower() for token in sentence]
+        tokens = [token.lower() for token in used_sentence]
 
     # Add <SOS> and <EOS> in beginning and end respectively
     tokens.insert(0, german.init_token)
@@ -33,7 +33,7 @@ def translate_sentence(model, sentence, german, english, device, max_length=50):
     text_to_indices = [german.vocab.stoi[token] for token in tokens]
 
     # Convert to Tensor
-    sentence_tensor = torch.LongTensor(text_to_indices).unsqueeze(1).to(device)
+    sentence_tensor = torch.LongTensor(text_to_indices).unsqueeze(1).to(used_device)
 
     # Build encoder hidden, cell state
     with torch.no_grad():
@@ -42,7 +42,7 @@ def translate_sentence(model, sentence, german, english, device, max_length=50):
     outputs = [english.vocab.stoi["<sos>"]]
 
     for _ in range(max_length):
-        previous_word = torch.LongTensor([outputs[-1]]).to(device)
+        previous_word = torch.LongTensor([outputs[-1]]).to(used_device)
 
         with torch.no_grad():
             output, hidden, cell = model.decoder(previous_word, hidden, cell)
@@ -59,7 +59,7 @@ def translate_sentence(model, sentence, german, english, device, max_length=50):
     # remove start token
     return translated_sentence[1:]
 
-def bleu(data, model, german, english, device):
+def bleu(data, model, german, english, used_device):
     """
     Calculates the BLEU(Bilingual Evaluation Understudy) score for a given sentence
 
@@ -67,7 +67,7 @@ def bleu(data, model, german, english, device):
     :param model:
     :param german:
     :param english:
-    :param device:
+    :param used_device:
     :return:
     """
     targets = []
@@ -77,7 +77,7 @@ def bleu(data, model, german, english, device):
         source = vars(example)['src']
         target = vars(example)['tgt']
 
-        prediction = translate_sentence(model, source, german, english, device)
+        prediction = translate_sentence(model, source, german, english, used_device)
         prediction = prediction[:-1] #remove <eos> token
 
         targets.append(target)
@@ -106,7 +106,7 @@ def load_checkpoint(checkpoint, model, optimizer):
     model.load_state_dict(checkpoint["state_dict"])
     optimizer.load_state_dict(checkpoint["optimizer"])
 
-def bleu_score_test(test_data, model, german, english):
+def bleu_score_test(test_data, model, german, english, used_device):
     """
     Make the BLEU score test
     :param test_data:
@@ -115,5 +115,5 @@ def bleu_score_test(test_data, model, german, english):
     :param english:
     :return:
     """
-    score = bleu(test_data[1:100], model, german, english, device)
+    score = bleu(test_data[1:100], model, german, english, used_device)
     return f'Bleu score: {score*100:.2f}'
